f130688355f5a4df222ef65bb3a4d0d76b6aa802
casperOne <casperOne@caspershouse.com>
2017-03-02 16:15:30 -0500

    Initial commit.

---
+++ Ofl.Google.DependencyInjection.Autofac/DefaultModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Autofac;
+
+namespace Ofl.Google.DependencyInjection.Autofac
+{
+    public class DefaultModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // Validate parameters.
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            // Bind.
+            builder.RegisterType<ConfigurationApiKeyProvider>().As<IApiKeyProvider>();
+        }
+    }
+}
